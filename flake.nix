356cfa5dc56fda9a5be30456a4b95777a3d9019e
bryce <bryce.hoehn@icloud.com>
2026-05-09 16:20:13 -0400

    feat: add debug image variant with shared base-debug layer

--- flake.nix
+++ flake.nix
@@ -11,22 +11,32 @@
     system = builtins.currentSystem;
     pkgs = nixpkgs.legacyPackages.${system};
     n2c = nix2container.outputs.packages.${system}.nix2container;
+    imageConfig = {
+      ExposedPorts = {
+        "3306/tcp" = {};
+      };
+      Volumes = {
+        "/var/lib/mysql" = {};
+      };
+      Cmd = [ "${pkgs.mariadb}/bin/mariadb" ];
+    };
   in {
     packages.${system} = {
       mariadb-image = n2c.buildImage {
         name = "mariadb";
         tag = "latest";
         fromImage = base.packages.${system}.base-image;
-        config = {
-          ExposedPorts = {
-            "3306/tcp" = {};
-          };
-          Volumes = {
-            "/var/lib/mysql" = {};
-          };
-          Cmd = [ "${pkgs.mariadb}/bin/mariadb" ];
-        };
+        config = imageConfig;
       };
+
+      mariadb-debug-image = n2c.buildImage {
+        name = "mariadb";
+        tag = "latest-debug";
+        fromImage = base.packages.${system}.base-debug-image;
+        config = imageConfig;
+      };
+
+      mariadb = pkgs.mariadb;
 
       default = self.packages.${system}.mariadb-image;
     };
